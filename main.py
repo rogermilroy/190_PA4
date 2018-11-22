83bb31d706bdd1c0d1de92f58a9793978c269648
rogermilroy <rogermilroy@icloud.com>
2018-11-21 22:05:04 -0800

    Implement process_train_data.

--- main.py
+++ main.py
@@ -9,23 +9,26 @@ import matplotlib.pyplot as plt
 # from configs import cfg
 import pandas as pd
 # from nltk.translate import bleu_score
-import utilities
+from utilities import *
+from beer_dataloader import *
 
 
-def load_data(fname):
-    # TODO: From the csv file given by filename and return a pandas DataFrame of the read csv.
-    data = pd.read_csv(open(fname, 'r'))
-    to_drop = ['Unnamed: 0', 'beer/name', 'beer/beerId', 'beer/brewerId', 'beer/ABV',
-               'review/appearance', 'review/aroma', 'review/palate', 'review/taste',
-               'review/time', 'review/profileName']
-    data = data.drop(columns=to_drop)
-    return data
-
-
-def process_train_data(data):
-    # TODO: Input is a pandas DataFrame and return a numpy array (or a torch Tensor/ Variable)
-    # that has all features (including characters in one hot encoded form).
-    raise NotImplementedError
+def process_train_data(texts, beers, ratings, character_only=False):
+    """
+    Processes a minibatch into one-hot encoding ready for input to the network.
+    :param texts:
+    :param beers:
+    :param ratings:
+    :param character_only:
+    :return:
+    """
+    data = texts2oh(texts)
+    # if we are not just training the language model.
+    if not character_only:
+        # concatenate text and metadata.
+        metadatas = get_metadatas(beers, ratings)
+        data = concat_metadatas(data, metadatas)
+    return to_tensor(data)
 
     
 def train_valid_split(data, labels):
@@ -40,14 +43,6 @@ def process_test_data(data):
     # have to worry about one hot encoding the data.
     raise NotImplementedError
 
-    
-def pad_data(orig_data):
-    # TODO: Since you will be training in batches and training sample of each batch may have reviews
-    # of varying lengths, you will need to pad your data so that all samples have reviews of length
-    # equal to the longest review in a batch. You will pad all the sequences with <EOS> character 
-    # representation in one hot encoding.
-    raise NotImplementedError
-    
 
 def train(model, X_train, y_train, X_valid, y_valid, cfg):
     # TODO: Train the model!
@@ -72,15 +67,20 @@ if __name__ == "__main__":
     test_data_fname = data_dir + "/Beeradvocate_Test.csv"
     out_fname = ""
 
-    train_data = load_data(train_data_fname) # Generating the pandas DataFrame
-    test_data = load_data(test_data_fname) # Generating the pandas DataFrame
-    train_data = utilities.add_limiters(train_data) # for testing.
-    tes = train_data['beer/style'][0]
-    print(train_data.iloc[0])
-    print(utilities.scale_beer_rating(train_data['review/overall'][0]))
-    print(utilities.beer2oh(tes))
-    print(utilities.oh2beer(utilities.beer2oh(tes)))
+    # train_data = load_data(train_data_fname) # Generating the pandas DataFrame
+    # test_data = load_data(test_data_fname) # Generating the pandas DataFrame
+    # train_data = utilities.add_limiters(train_data) # for testing.
+    # tes = train_data['beer/style'][0]
+    # print(train_data.iloc[0])
+    # print(utilities.scale_beer_rating(train_data['review/overall'][0]))
+    # print(utilities.beer2oh(tes))
+    # print(utilities.oh2beer(utilities.beer2oh(tes)))
 
+    train_loader, val_loader = create_split_loaders(2, 42, train_data_fname)
+    text1, beers1, rating1 = iter(train_loader).next()
+    print(text1, beers1, rating1)
+    batch = process_train_data(text1, beers1, rating1, True)
+    print(batch)
     # train_data, train_labels = process_train_data(train_data) # Converting DataFrame to numpy array
     # X_train, y_train, X_valid, y_valid = train_valid_split(train_data, train_labels) # Splitting the train data into train-valid data
     # X_test = process_test_data(test_data) # Converting DataFrame to numpy array
