7b095b5e586ce517aecefa2ef08423d27bd365d0
Anh Ngo <adn057@ucsd.edu>
2018-11-29 14:57:15 -0800

    add gru and option in config to choose model

--- models.py
+++ models.py
@@ -53,3 +53,50 @@ class baselineLSTM(nn.Module):
         out = self.hidden2out(out)
         return out
 
+class gru(nn.Module):
+    def __init__(self, config):
+        super(gru, self).__init__()
+
+        #Initialize your layers and variables that you want;
+        # Keep in mind to include initialization for initial hidden states of LSTM, you
+        # are going to need it, so design this class wisely.
+        self.input_dim = cfg['input_dim']
+        self.hidden_dim = cfg['hidden_dim']
+        self.output_dim = cfg['output_dim']
+        self.layers = cfg['layers']
+        self.dropout = cfg['dropout']
+        self.bidirectional = cfg['bidirectional']
+        self.batch_size = cfg['batch_size']
+        self.cell_state = None
+        self.hidden_state = None
+
+        # initialize GRU layer
+        self.gru = nn.GRU(input_size=self.input_dim,
+                            hidden_size=self.hidden_dim,
+                            num_layers=self.layers,
+                            dropout=self.dropout,
+                            bidirectional=self.bidirectional)
+
+        # initialize output layer
+        self.hidden2out = nn.Linear(self.hidden_dim, self.output_dim)
+
+    def reset_hidden(self):
+        self.cell_state = None
+        self.hidden_state = None
+
+    def forward(self, sequence):
+        # Takes in the sequence of the form (1 x batch_size x input_dim) and
+        # returns the output of form (1 x batch_size x output_dim)
+
+        hidden = (self.hidden_state, self.cell_state)
+        if self.cell_state is None or self.hidden_state is None:
+            out, states = self.gru(sequence)
+        else:
+            out, states = self.gru(sequence, hidden)
+        self.hidden_state = states[0]
+        self.cell_state = states[1]
+
+        out = self.hidden2out(out)
+        return out
+
+
