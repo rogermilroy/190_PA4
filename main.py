2214760db941e5dd1e56934fa51c7705c86c3c3a
rogermilroy <rogermilroy@icloud.com>
2018-12-01 20:30:47 -0800

    Load existing params.

--- main.py
+++ main.py
@@ -241,6 +241,8 @@ if __name__ == "__main__":
         extras = False
         print("CUDA NOT supported")
 
+    model.load_state_dict(torch.load('./outputs/current_params.pt', map_location=computing_device))
+
     train_loader, val_loader = create_split_loaders(cfg['batch_size'], 42, train_data_fname,
                                                     extras=extras, subset=True, p_val=0.1)
 
