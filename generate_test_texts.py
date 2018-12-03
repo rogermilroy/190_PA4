78f6df5f872497808b1e42725f2b6116b74f0491
rogermilroy <rogermilroy@icloud.com>
2018-12-02 19:43:08 -0800

    Change generation code to reflect changed spec.

--- generate_test_texts.py
+++ generate_test_texts.py
@@ -1,23 +1,24 @@
-from main import generate, process_test_data, save_to_file
-import configs
+from main import generate, process_test_data, process_train_data, save_to_file
+from configs import *
 from beer_dataloader import create_generation_loader
 import sys
 from models import *
+from utilities import *
 import torch
 
 if __name__ == '__main__':
 
-    data_dir = "../BeerAdvocateDataset"
+    data_dir = "datasets/cs190f-public/BeerAdvocateDataset"
     train_data_fname = data_dir + "/Beeradvocate_Train.csv"
-    test_data_fname = data_dir + "/Beeradvocate_Test.csv"
+    test_data_fname = data_dir + "/Beeradvocate_TestOriginal.csv"
 
-    modeltype = cfg['model'].lower()
+    modeltype = gen_cfg['model'].lower()
     if (modeltype == 'lstm' or modeltype == 'baselinelstm'):
         print("Using LSTM Model")
-        model = baselineLSTM(cfg)
+        model = baselineLSTM(gen_cfg)
     elif (modeltype == 'gru'):
         print("Using GRU Model")
-        model = gru(cfg)
+        model = gru(gen_cfg)
     else:
         print("Model ", modeltype, " not found")
         sys.exit()
@@ -26,7 +27,7 @@ if __name__ == '__main__':
     use_cuda = torch.cuda.is_available()
 
     # Setup GPU optimization if CUDA is supported
-    if use_cuda and cfg['cuda']:
+    if use_cuda and gen_cfg['cuda']:
         computing_device = torch.device("cuda")
         extras = {"num_workers": 4, "pin_memory": True}
         print("CUDA is supported")
@@ -38,17 +39,41 @@ if __name__ == '__main__':
     model.to(computing_device)
     model.load_state_dict(torch.load('./outputs/current_params.pt', map_location=computing_device))
 
-    test_loader = create_generation_loader(cfg['batch_size'], test_data_fname, extras=extras)
+    test_loader = create_generation_loader(gen_cfg['batch_size'], test_data_fname, extras=extras)
 
-    texts = []
+    # texts = []
+    # with torch.no_grad():
+    #     for minibatch_count, (beers, ratings) in enumerate(test_loader, 0):
+    #         batch = process_test_data(beers, ratings, computing_device)
+    #         text = generate(model, batch, gen_cfg, computing_device)
+    #         print(text[0])
+    #         texts.extend(text)
 
-    for minibatch_count, (beers, ratings) in enumerate(test_loader, 0):
-        batch = process_test_data(beers, ratings, computing_device)
-        text = generate(model, batch, cfg, computing_device)
-        print(text[0])
-        for t in text:
-            texts.append(t)
+    val_samples = 0
+    bleu_score_avg = 0.
+    with torch.no_grad():
+        for val_minibatch_count, (val_text, val_beer, val_rating) in enumerate(test_loader, 0):
+            val_samples += len(val_beer)
 
-    save_to_file(texts, './outputs/reviews_tau_' + cfg['gen_temp'] + '_' + modeltype + '.txt')
+            # validation
+            validation_loss = 0
+            model.reset_hidden()
+
+
+            # generate reviews and check bleu scores.
+            generated_val_reviews = generate(model, process_test_data(val_beer,
+                                                                      val_rating,
+                                                                      computing_device),
+                                             gen_cfg,
+                                             computing_device)
+            b_scores = torch.tensor(get_bleu_scores(generated_val_reviews, val_text))
+            bleu_score_avg += float(torch.mean(b_scores))
+            if val_samples > 2000:
+                break
+
+        bleu_score_avg = (bleu_score_avg / float(val_samples))
+        print("BLEU score: ", bleu_score_avg)
+
+    # save_to_file(texts, './outputs/reviews_tau_' + gen_cfg['gen_temp'] + '_' + modeltype + '.txt')
 
 
